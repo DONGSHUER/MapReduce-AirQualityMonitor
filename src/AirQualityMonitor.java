a56fef73b5f464ae73906fd8ab69f270ebe77dbb
DONGSHUER <16251164@life.hkbu.edu.hk>
2021-02-28 15:23:25 +0800

    Commit of Finish the Lab 3 - Task 1

--- src/AirQualityMonitor.java
+++ src/AirQualityMonitor.java
@@ -1,16 +1,12 @@
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
+import com.sun.xml.internal.txw2.TxwException;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.examples.SecondarySort;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -25,18 +21,16 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class AirQualityMonitor {
 
-    public static final String LEVEL_0 = "Invalid data";
-    public static final String LEVEL_1 = "Good";
-    public static final String LEVEL_2 = "Moderate";
-    public static final String LEVEL_3 = "Unhealthy for Sensitive Group";
-    public static final String LEVEL_4 = "Unhealthy";
-    public static final String LEVEL_5 = "Very Unhealthy";
-    public static final String LEVEL_6 = "Hazardous";
+    public static final String LEVEL_0 = "Invalid data ";
+    public static final String LEVEL_1 = "Good ";
+    public static final String LEVEL_2 = "Moderate ";
+    public static final String LEVEL_3 = "Unhealthy for Sensitive Group ";
+    public static final String LEVEL_4 = "Unhealthy ";
+    public static final String LEVEL_5 = "Very Unhealthy ";
+    public static final String LEVEL_6 = "Hazardous ";
 
     public static class AirQualityMapper
             extends Mapper<LongWritable, Text, Text, Text> {
-
-        private Text output = new Text();
 
         @Override
         public void map(LongWritable key, Text value, Context context
@@ -51,6 +45,7 @@ public class AirQualityMonitor {
             String ym = itr.nextToken(); // get the first column - year month indicator
             int yearMonth = Integer.parseInt(ym);
 
+            String output = new String();
             if (yearMonth == 0) {
                 return; // skip the first row 0
             } else {
@@ -58,25 +53,23 @@ public class AirQualityMonitor {
                     String sVal = itr.nextToken(); // get the actual value
                     int val = Integer.parseInt(sVal);
                     if (val >= 0 && val <= 49) {
-                        output.set("Good");
+                        output += LEVEL_1;
                     } else if (val >= 50 && val <= 99) {
-                        output.set("Moderate");
+                        output += LEVEL_2;
                     } else if (val >= 100 && val <= 149) {
-                        output.set("Unhealthy for Sensitive Group");
+                        output += LEVEL_3;
                     } else if (val >= 150 && val <= 199) {
-                        output.set("Unhealthy");
+                        output += LEVEL_4;
                     } else if (val >= 200 && val <= 299) {
-                        output.set("Very Unhealthy");
+                        output += LEVEL_5;
                     } else if (val >= 300) {
-                        output.set("Hazardous");
+                        output += LEVEL_6;
                     } else if (val == -1) {
-                        output.set("invalid data");
+                        output += LEVEL_0;
                     }
-                    context.write(new Text(ym), output);
                 }
-
+                context.write(new Text(ym), new Text(key.toString() + ":" + output));
             }
-
 
         }
     }
@@ -84,17 +77,38 @@ public class AirQualityMonitor {
     public static class AirQualityReducer
             extends Reducer<Text, Text, Text, Text> {
 
+        private String new_key = new String();
+        private String output = new String();
+
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
 
-            for (Text val : values) {
-                context.write(key, new Text(val));
-            }
-            // for each val in values
-            // context.write(key, val)
-        }
+            /*
+            Re-order (in ascending) according do the offset -> get the corresponding day (also in ascending) -> append day to the year month -> print to key on reducer side
+             */
+            Map<Integer, String> map = new TreeMap<>();
 
+            for (Text val : values) {
+                String pre_output = val.toString();
+                String[] arr = pre_output.split(":");
+
+                String v_offset = arr[0];
+                int v_offset_int = Integer.parseInt(v_offset);
+                String out = arr[1];
+                map.put(v_offset_int, out);
+            }
+
+            List<Integer> offsetByKey = new ArrayList<>(map.keySet());
+            Collections.sort(offsetByKey);
+
+            for(int i = 0; i < offsetByKey.size(); i++) {
+                new_key = key.toString() + ":" + (i + 1);
+                output = map.get(offsetByKey.get(i));
+                context.write(new Text(new_key), new Text(output));
+            }
+
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -127,3 +141,4 @@ public class AirQualityMonitor {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
+
